f79443949a72642207ada919e1e77a0e092ae274
João Paulo Lazari <134349377+joaoLazari@users.noreply.github.com>
2023-06-06 00:35:19 -0300

    Add files via upload

---
+++ team1.js
@@ -0,0 +1,23 @@
+// Ejercicio 3: Validación de contraseña
+// el programa debe recibir una contraseña. 
+// Aplica las siguientes condiciones de validación:
+
+// La contraseña debe tener al menos 8 caracteres.
+// La contraseña debe contener al menos un número.
+// La contraseña debe contener al menos una letra mayúscula.
+
+let contrasenha = "A1jjjjjj";
+
+function validacion(str){
+    if(str.length >= 8){
+        if(/\d/.test(str)) {
+            if(/[A-Z]/.test(str)){
+                console.log("Tienes acceso");
+            }
+        }    
+    }else {
+        console.log("No tienes acceso")
+    }
+}
+
+validacion(contrasenha)
